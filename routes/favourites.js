34d3d88a4d01c97c47b639e8d8c18b5ba749ed51
Joshua Cheung <contact.cjoshua@gmail.com>
2022-06-04 12:40:54 -0700

    complete favourites route

---
+++ routes/favourites.js
@@ -0,0 +1,58 @@
+// We may need change the req.params to req.session in future
+// Need to connect to tag table
+
+const express = require("express");
+const router = express.Router();
+
+module.exports = (db) => {
+  // Get - favourite with 'id'
+  router.get("/:id", (req, res) => {
+    const id = req.params.id;
+    db.query(`SELECT * FROM favourites WHERE id = ${id};`)
+      .then((data) => {
+        const resources = data.rows; // will return array of object
+        // res.json({ resources });
+        console.log(resources);
+      })
+      .catch((err) => {
+        res.status(500).json({ error: err.message });
+      });
+  });
+
+  // Post - ADD favourite
+  router.post("/", (req, res) => {
+    // need to find out how grab data from req
+    const user_id = req.params.id;
+    const resource_id = req.params.resource_id;
+
+    // insert table
+    db.query(
+      `INSERT INTO favourites (user_id, resource_id) 
+              VALUES (${user_id}, ${resource_id});`
+    )
+      .then((data) => {
+        console.log(data.rows);
+      })
+      .catch((err) => {
+        res.status(500).json({ error: err.message });
+      });
+  });
+
+  // Post - Delete favourite
+  router.post("/:id/delete", (req, res) => {
+    const id = req.params.id;
+    //delete query
+    db.query(
+      `DELETE FROM favourite 
+              WHERE id = ${id}`
+    )
+      .then((data) => {
+        console.log("delete success");
+      })
+      .catch((err) => {
+        res.status(500).json({ error: err.message });
+      });
+  });
+
+  return router;
+};
