2d83226653a90ce1b19360b4bbba1ba89db7e872
James Ball <jamie.j.ball@gmail.com>
2022-06-03 22:02:34 -0400

    still testing

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,9 +1,20 @@
 -- Users table seeds here (Example)
-<<<<<<< HEAD
-INSERT INTO users (name) VALUES ('Alice');
-INSERT INTO users (name) VALUES ('Kira');
+INSERT INTO
+  users (name)
+VALUES
+  ('Alice');
 
-INSERT INTO users (name) Values ('steve');
-=======
+INSERT INTO
+  users (name)
+VALUES
+  ('Kira');
 
->>>>>>> 2ed346cd9ecd531197998e60fffe20e0437874f8
+INSERT INTO
+  users (name)
+VALUES
+  ('steve');
+
+INSERT INTO
+  users (name)
+VALUES
+  ('joe');
