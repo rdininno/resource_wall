5d4537be9929559401a826229a8528a8e764f5c3
James Ball <jamie.j.ball@gmail.com>
2022-06-06 19:45:24 -0400

    updated query to fix duplication bug

--- routes/databaseQueries.js
+++ routes/databaseQueries.js
@@ -3,58 +3,43 @@ module.exports = function (db) {
     getAllResources: (limit = 10) => {
       const allResourcesQuery = `SELECT * FROM resources LIMIT $1`;
       const allResourcesData = [limit];
-      return db.query(allResourcesQuery, allResourcesData)
+      return db
+        .query(allResourcesQuery, allResourcesData)
         .then((res) => {
-          console.log("allResources:", res.rows);
           return res.rows;
         })
         .catch((err) => {
           console.log("error in allResources:", err);
         });
-    }
+    },
+
+    resourceSearchQuery: (options, limit = 10) => {
+      const queryData = options.body.data;
+      let queryString = `
+          SELECT resources.* FROM resources
+          WHERE 1=1`;
+      const queryParams = [];
+
+      queryString += ` AND title ILIKE $${queryParams.length + 1
+        } OR description ILIKE $${queryParams.length + 1}`;
+      queryParams.push(`%${queryData}%`);
 
 
+      queryParams.push(limit);
+      queryString += `
+          GROUP BY resources.title, resources.id
+          ORDER BY resources.created_at
+          LIMIT $${queryParams.length}`;
 
-
-
-
-
-  };
-
-  const resourceSearchQuery = (limit = 10) => {
-    const options = searchSubmit();
-    let queryString = `
-        SELECT * FROM resurces
-        JOIN users ON resources.user_id = users.id
-        JOIN tags ON resources.id = tags.resource_id
-        WHERE 1=1`;
-    const queryParams = [];
-
-    if (options.search) {
-      queryString += ` AND title LIKE $${queryParams.length + 1
-        } OR description LIKE $${queryParams.length + 1}`;
-      queryParams.push(`%${options.search}%`);
-    }
-
-    if (options.tag) {
-      queryString += ` AND tag LIKE $${queryParams.length + 1}`;
-      queryParams.push(`%${options.tags}%`);
-    }
-
-    queryParams.push(limit);
-    queryString += `
-        GROUP BY resources.title
-        ORDER BY resources.created_at
-        LIMIT $${queryParams.length}`;
-
-    return db
-      .query(queryString, queryParams)
-      .then((res) => {
-        return res.rows;
-      })
-      .catch((err) => {
-        console.log("error in searchQuery:", err);
-        return null;
-      });
+      return db
+        .query(queryString, queryParams)
+        .then((res) => {
+          return res.rows;
+        })
+        .catch((err) => {
+          console.log("error in searchQuery:", err);
+          return null;
+        });
+    },
   };
 };
