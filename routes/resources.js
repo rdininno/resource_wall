542109d023f4d6ce12250d5fa9a9569dbf2f711a
Robert Di Ninno <robertdininno@gmail.com>
2022-06-07 20:13:13 +0000

    add send query insert for tags from submission form

--- routes/resources.js
+++ routes/resources.js
@@ -15,7 +15,7 @@ module.exports = (db) => {
     //send res for testing
 
     console.log(`hello from resources: id ${id}`);
-    db.query(`select * from resources where creator_id = ${id};`)
+    db.query(`select * from resources where creator_id = '1';`)
       .then((data) => {
         console.log(data.rows);
         return res.send(data.rows);
@@ -30,15 +30,20 @@ module.exports = (db) => {
     const url = req.body.url;
     const title = req.body.title;
     const description = req.body.description;
+    const tags = req.body.tags;
 
     // insert table
     return db
       .query(
-        `INSERT INTO resources (creator_id, title, description, url) VALUES ('1', $1, $2, $3)`,
-        [title, description, url]
+        `INSERT INTO resources (creator_id, title, description, url) VALUES ('1', $1, $2, $3) RETURNING *;`,
+        [title, description, url],
+
       )
       .then((res) => {
-        return res.rows[0];
+        const newResource = res.rows[0];
+        return db.query(
+          `INSERT INTO tags (user_id, resource_id, tag) VALUES ('1', $1, $2)`,
+        [newResource.id, tags])
       })
       .catch((err) => {
         console.log(err);
