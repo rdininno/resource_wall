ffd59a95e33c4a12de71928d186e9bf405264977
Robert Di Ninno <robertdininno@gmail.com>
2022-06-07 03:57:13 +0000

    add alert hide and show for submission

--- public/scripts/newResource.js
+++ public/scripts/newResource.js
@@ -1,11 +1,12 @@
 $(document).ready(() => {
+  $('.added_alert').hide();
   $('#create_new_resource_form').on('submit', (event) => {
     event.preventDefault();
     const formData = $(event.target).serialize();
 
     $.post('/resources', formData).then((response) => {
-      console.log('success: ', response);
+      console.log('success', response);
     })
-
+    $('.added_alert').show('fast');
   })
 });
