5977b1fe3f7c802d936af45431ef26ef3df2f2b0
James Ball <jamie.j.ball@gmail.com>
2022-06-08 09:41:56 -0400

    add tailwind dependency

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  content: ["./views/**/*.ejs"],
+  theme: {
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
+    extend: {
+      colors: {
+        "dark-red": "#780000",
+        "reg-red": "#C1121F",
+        cream: "#FDF0D5",
+        "dark-cream": "#FBE3B4",
+        "dark-blue": "#003049",
+        "reg-blue": "#669BBC",
+      },
+    },
+  },
+  plugins: [],
+};
