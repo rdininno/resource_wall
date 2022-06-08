3e1af2ea286e7b82a727042de03df258b302e3ff
James Ball <jamie.j.ball@gmail.com>
2022-06-08 17:17:02 -0400

    add edit resource form routes

--- public/scripts/resourcePage.js
+++ public/scripts/resourcePage.js
@@ -1,36 +1,51 @@
 $(document).ready(() => {
-  $("#review_form").on("submit", (event) => {
-    event.preventDefault();
-    let resourceId = window.location.pathname;
-    resourceId = resourceId.split("/");
-    resourceId = resourceId[resourceId.length - 1];
-
-    const comment = $(".commentBoxInput").val();
-    let $input = $("#review_form :input");
-    let tagValue;
-
-    for (const i of $input) {
-      if (i.type === "radio") {
-        if (i.checked) {
-          tagValue = i.value;
-        }
-      }
-    }
-
-    const data = {
-      comment,
-      rating: tagValue,
-    };
-
-    $.post(`/reviews/${resourceId}`, data);
-  });
+  $("#review_form").on("submit", addReview);
 
   // Delete button onclick
   $("#delete_button").on("click", deleteResource);
 
   // Like  button onclick
   $("#like_button").on("click", addFavourite);
+
+  $("#edit_resource_form").on("submit", editResource);
 });
+
+const editResource = (evt) => {
+  evt.preventDefault();
+  let resourceId = getPath();
+  const data = $(evt.target).serialize();
+
+  $.post(`/resources/${resourceId}`, data)
+    .then(() => {
+      location.reload();
+    })
+    .catch((err) => {
+      console.log("error on edit resource", err);
+    });
+};
+
+const addReview = function (event) {
+  event.preventDefault();
+  let resourceId = getPath();
+  const comment = $(".commentBoxInput").val();
+  let $input = $("#review_form :input");
+  let tagValue;
+
+  for (const i of $input) {
+    if (i.type === "radio") {
+      if (i.checked) {
+        tagValue = i.value;
+      }
+    }
+  }
+
+  const data = {
+    comment,
+    rating: tagValue,
+  };
+
+  $.post(`/reviews/${resourceId}`, data);
+};
 
 // Get URL param
 const getPath = function () {
