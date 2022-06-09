504e87747a2bfd3ebd0461d9fb98623590826b02
Joshua Cheung <contact.cjoshua@gmail.com>
2022-06-08 19:51:23 -0700

    clean up old code

--- public/scripts/loginbutton.js
+++ public/scripts/loginbutton.js
@@ -8,49 +8,12 @@ $(() => {
     const user_id = $(e.target).val();
     $.post(`/users/set/${user_id}`).then(() => {
       location.reload();
-      // if (window.location.pathname === "/") {
-      //   location.reload();
-      // }
-      // $.ajax({
-      //   type: "GET",
-      //   url: "/users",
-      //   success: function () {
-      //     $(".userLogin").replaceWith(buttonTemplate(user_id));
-      //   },
-      // });
     });
-    // $.ajax({
-    //   type: "GET",
-    //   url: `/users`,
-    //   success: function () {
-    //     $(".home_title").text("My Resources");
-    //     if (window.location.pathname === "/") {
-    //       $.ajax({
-    //         type: "GET",
-    //         url: "/",
-    //       });
-    //     }
-    //     loadResources();
-    //   },
-    // });
-
-    // $(".userLogin").css("display", "none");
   });
 
   $(".logout").click(function () {
     $.post("/users/logout").then(() => {
       location.reload();
     });
-
-    // $.ajax({
-    //   type: "GET",
-    //   url: `/users`,
-    //   success: function () {
-    //     $(".home_title").text("My Resources");
-    //     loadResources();
-    //   },
-    // });
-    // $(".logout").css("display", "none");
-    // $(".loginButton").css("display", "block");
   });
 });
